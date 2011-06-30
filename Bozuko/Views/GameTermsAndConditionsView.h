fe143626967f8e3aabdce13ac5e0af8102f6de99
Tom Corwine <tc@corwine.org>
2011-06-29 23:56:32 -0400

    Release Version 1.1

---
+++ Bozuko/Views/GameTermsAndConditionsView.h
@@ -0,0 +1,19 @@
+//
+//  GameTermsAndConditionsView.h
+//  Bozuko
+//
+//  Created by Tom Corwine on 6/2/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class BozukoGame;
+
+@interface GameTermsAndConditionsView : UIView {
+    
+}
+
+- (id)initWithGame:(BozukoGame *)inBozukoGame;
+
+@end
